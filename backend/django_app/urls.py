b6855200934bc543bbd002527b60c2e77fa97e2d
Diya <diya70559@gmail.com>
2025-03-10 21:40:42 +0530

    Resolved conflicts

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -1,24 +1,9 @@
 from django.contrib import admin
 from django.urls import path , include
 from django.http import JsonResponse
-from django.views.decorators.cache import never_cache
-
-@never_cache
-def hello_name(request):
-    """
-    A simple view that returns 'Hey! I'm {name} from {city}' in JSON format.
-    Uses a query parameter named 'name' and 'city'.
-    """
-    # Get 'name' from the query string, default to 'World' if missing
-    name = request.GET.get("name", "dark")
-    city = request.GET.get("city", "Peter Pan")
-    return JsonResponse({"message": f"Hey! I'm {name} from {city} :)"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_name),
     path('product/', include("product.urls"))
-    # Example usage: /hello/?name=Bob
-    # returns {"message": "Hello, Bob!"}
 ]
 
